2d9c8df3a2f627bbc1f1340dd599a4f6e3bdf9a7
Syed Amjad Ali <143636893+Syed-Amjad@users.noreply.github.com>
2025-06-14 20:14:58 +0500

    Update LLM model

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def generate_questions(client):
                     "content": "Create 20 multiple-choice questions with 4 options each about Git (introduction, add, commit, stash, branch, merge, push, pull, pull requests) and identify the correct answer for each."
                 }
             ],
-            model='llama3-70b-versatile'
+            model="meta-llama/llama-4-scout-17b-16e-instruct"
 
         )
         response = chat_completion.choices[0].message.content
